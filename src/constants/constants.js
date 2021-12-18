15151082c7272d2e5b71d6bf4d4993efee015a35
GokulRajS1998 <gokul.shibi10@gmail.com>
2021-12-18 18:25:42 +1100

    netflix commit

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,3 @@
-export const baseUrl ='https://api.themoviedb.org/3';
-export const API_KEY ='0cc60a8b3bb44f2978041f193a721e39';
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY='0cc60a8b3bb44f2978041f193a721e39';
+export const imageUrl='https://image.tmdb.org/t/p/original'
